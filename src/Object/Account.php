27b1aecb1e00e4d5fe30972e6396cff5a0a80faf
Quezler <Quezler@me.com>
2017-11-22 12:47:50 +0100

    Cast timestamps to carbon

--- src/Object/Account.php
+++ src/Object/Account.php
@@ -2,6 +2,7 @@
 
 namespace Quezler\OnePasswordPhpApi\Object;
 
+use Carbon\Carbon;
 use stdClass;
 
 class Account
@@ -23,5 +24,7 @@ class Account
         foreach (get_object_vars($object) as $key => $value) {
             $this->$key = $value;
         }
+
+        $this->createdAt = new Carbon($this->createdAt);
     }
 }
