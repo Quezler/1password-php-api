944367e07e2238ce6f84cef1fe7f0341272bca2c
Quezler <Quezler@me.com>
2017-11-22 09:40:26 +0100

    Crawl all download links from the most recent release

--- src/Console/DownloadOpCommand.php
+++ src/Console/DownloadOpCommand.php
@@ -6,6 +6,7 @@ use GuzzleHttp\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
 
 class DownloadOpCommand extends Command
 {
@@ -38,7 +39,40 @@ class DownloadOpCommand extends Command
         $this->output = $output;
 
         $this->detectOperatingSystem();
-        $this->getReleases();
+//        $this->getReleases();
+
+        $html = (new Client)->get(self::releases)->getBody()->getContents();
+
+        $crawler = new Crawler($html);
+
+        $releases = $crawler->filter('article[id]');
+
+        $mostRecentRelease = $releases->first();
+
+        $downloads = $mostRecentRelease->filter('a[href]');
+
+        $downloads->each(function (Crawler $anchor) {
+            dump($anchor->attr('href'));
+        });
+
+        /**
+        "#v11001"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_darwin_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_darwin_amd64_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_freebsd_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_freebsd_amd64_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_freebsd_arm_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_linux_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_linux_amd64_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_linux_arm_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_netbsd_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_netbsd_amd64_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_netbsd_arm_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_openbsd_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_openbsd_amd64_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_windows_386_v0.1.1.zip"
+        "https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_windows_amd64_v0.1.1.zip"
+         */
     }
 
     /**
@@ -53,9 +87,19 @@ class DownloadOpCommand extends Command
         $this->os = $osLow;
     }
 
-    private function getReleases() {
-        $html = (new Client)->get(self::releases)->getBody()->getContents();
-
-        dump($html);
-    }
+//    private function getReleases() {
+//        $html = (new Client)->get(self::releases)->getBody()->getContents();
+//
+//        $crawler = new Crawler($html);
+//
+//        $releases = $crawler->filter('article[id]');
+//
+//        $mostRecentRelease = $releases->first();
+//
+//        dump($mostRecentRelease->html());
+//
+////        $releases->each(function (Crawler $article) {
+////            dump($article->filter('h3')->text());
+////        });
+//    }
 }
